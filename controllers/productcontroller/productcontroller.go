d8a79918e46221ed117f21abcabf15fd07f58782
zikrikn <zikrikn@student.telkomuniversity.ac.id>
2023-07-08 02:05:27 +0700

    productcontrollers

--- controllers/productcontroller/productcontroller.go
+++ controllers/productcontroller/productcontroller.go
@@ -1,18 +1,20 @@
 package productcontroller
 
 import (
+	"encoding/json"
 	"go-simple-projects/models"
 	"net/http"
+	// "strconv"
+
+	// "fmt"
 
 	"github.com/gin-gonic/gin"
-	"fmt"
+	"gorm.io/gorm"
 )
 
 func Index(c *gin.Context){
 	var products []models.Product
 	//This is slice and can be used as array, so has a lot of product
-	//Learn about struct again
-	fmt.Println(products)
 
 	models.DB.Find(&products) //Learn more about pointer again
 
@@ -28,8 +30,6 @@ func Index(c *gin.Context){
 func Show(c *gin.Context){
 	var product models.Product
 	//While about this, this only one product
-
-	fmt.Println(product)
 
 	id := c.Param("id")
 
@@ -72,7 +72,7 @@ func Update(c *gin.Context){
 	c.JSON(http.StatusOK, gin.H{"product": product})
 }
 
-func Delete(c *gin.Context){
+func Delete(c *gin.Context) {
 	// What is *gin.Context?
 	// The gin.Context is the most important part of Gin. It carries request details, validates the request, stores the result of validation, and calls the appropriate handler to process the request.
 	// What is gin.H?
@@ -80,13 +80,29 @@ func Delete(c *gin.Context){
 
 	var product models.Product
 
-	id := c.Param("id")
+	var input struct {
+		ID json.Number `json:"id"`
+	}
 
-	if err := models.DB.Where("id = ?", id).First(&product).Error; err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "Record not found!"})
+	if err := c.ShouldBindJSON(&input); err != nil {
+		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
 
-	models.DB.Delete(&product)
+	id, err := input.ID.Int64()
+	if err != nil {
+		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	if err := models.DB.Delete(&product, id).Error; err != nil {
+		if err == gorm.ErrRecordNotFound {
+			c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": "Record not found!"})
+			return
+		}
+		c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"error": "Failed to delete product"})
+		return
+	}
+
 	c.JSON(http.StatusOK, gin.H{"product": "Product deleted!"})
 }
